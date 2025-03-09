5234f742aa3b8e4f5ef5afa47663ce0cfc55fd49
Andrei Fenesan <andrei.fenesan.8x8.com>
2025-03-09 09:11:44 +0200

    Added the -port flag when starting the server

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 
 func main() {
 	var rootDirectory string
+	var port int
 	flag.StringVar(&rootDirectory, "src", "./resources", "The root direcotory from which the resource will be served")
+	flag.IntVar(&port, "port", 8080, "The server port")
 	flag.Parse()
 
+	actualPort := uint32(port)
 	requestHandler := handler.NewHttpRequestHandler(rootDirectory)
-	connManager := manager.NewConcurrentConnectionManger(requestHandler, 8081)
-	log.Printf("The server will start using the root direcotry: %s\n", rootDirectory)
+	connManager := manager.NewConcurrentConnectionManger(requestHandler, actualPort)
+	log.Printf("The server will start on port: %d, using the root direcotry: %s\n", actualPort, rootDirectory)
 	connManager.Start()
 }
